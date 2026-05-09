51b83cff67a9d79e215704d6bb11f915d462598a
nzbin <nzb329@163.com>
2026-05-09 12:06:35 +0800

    chore: update tsconfig and small fix

--- projects/watermark/src/public-api.ts
+++ projects/watermark/src/public-api.ts
@@ -2,6 +2,6 @@
  * Public API Surface of watermark
  */
 
-export { WatermarkOptions } from './lib/types';
+export type { WatermarkOptions } from './lib/types';
 export * from './lib/watermark';
 export * from './lib/watermark.directive';
